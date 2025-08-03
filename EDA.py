5c1aaf08c6851ac9b386af73ae7a8fd37863c6f3
draxssup <dhruv.chatrani@gmail.com>
2025-08-03 15:56:34 +0530

    trimmed dataset

--- EDA.py
+++ EDA.py
@@ -3,16 +3,19 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-data = pd.read_csv("clean_jobs.csv")
+data = pd.read_csv("data/clean_jobs.csv")
 
 print(data.info())
 print((data.isnull().sum()/data['id'].count())*100)
 #irrelevent features
 data.drop(['location', 'link', 'source', 'id','work_type','employment_type'],axis=1,inplace=True)
-
-#saving new csv file:
-data.to_csv('newdata.csv', index=False)
-
+#replace na description with title
 data['description'] =  data['description'].fillna(data['title'])
-
+#replace company with unk
+data['company'] =  data['company'].fillna('Unknown')
+#removing all rows with no date data:
+data = data.dropna(subset='date_posted')
+#checking null values
 print((data.isnull().sum()/data['title'].count())*100)
+#saving cleaned data:
+data.to_csv('data/newdata.csv', index=False)
